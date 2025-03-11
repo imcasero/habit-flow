cc5aac0482aa8fb96a018638af597b3888f4c7bc
Imcasero <diegocaserosmr@gmail.com>
2025-03-11 20:24:56 +0100

    feat: add clean styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { ThemeToggle } from "@/shared/components/ThemeToggle/ThemeToggle";
 
 export default function Page() {
   return (
-    <div className="bg-white dark:bg-gray-900 min-h-screen">
+    <main className="bg-white dark:bg-black min-h-screen flex flex-col justify-center items-center p-4 gap-2">
       <ThemeToggle />
       <h3 className="text-black dark:text-white">Hola desde page</h3>
-    </div>
+    </main>
   );
 }
