b62379c416a4abe7b611e02ed225ac0a23ffeda5
Imcasero <diegocaserosmr@gmail.com>
2025-04-30 23:25:42 +0200

    refactor: remove CreateUser class as part of application restructuring

--- src/core/application/CreateUser.ts
+++
@@ -1,9 +0,0 @@
-import { AuthRepository } from "@/infra/repositories/AuthRepository";
-import { User } from "../domain/dto/auth";
-import { Session } from "@supabase/supabase-js";
-
-export class CreateUser {
-  static async execute(newUserData: User): Promise<Session> {
-    return await AuthRepository.registerUser(newUserData);
-  }
-}
