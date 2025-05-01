67adbc710ab1fd5f79a3a25320eee4cbe7822246
Imcasero <diegocaserosmr@gmail.com>
2025-05-01 15:13:19 +0200

    feat: update LoginForm to use Input and Checkbox components for improved structure

--- src/ui/components/auth/LoginForm/LoginForm.tsx
+++ src/ui/components/auth/LoginForm/LoginForm.tsx
@@ -1,3 +1,7 @@
+import { Checkbox } from "@/ui/shared/components/Checkbox/Checbox";
+import { Input } from "@/ui/shared/components/Input/Input";
+import Link from "next/link";
+
 export const LoginForm = () => {
   return (
     <div className="max-w-md mx-auto border border-gray-300 dark:border-gray-700 rounded-md pt-4 pb-8 px-4 flex flex-col gap-4 itemsp-center">
@@ -8,33 +12,9 @@ export const LoginForm = () => {
         </p>
       </div>
       <form className="flex flex-col gap-6">
-        <label className="flex flex-col gap-1">
-          <span className="text-gray-700 dark:text-gray-400 px-0.5">
-            Username
-          </span>
-          <input
-            type="text"
-            placeholder="Username"
-            className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white transition-colors duration-200"
-          />
-        </label>
-        <label className="flex flex-col gap-1">
-          <span className="text-gray-700 dark:text-gray-400 px-0.5">
-            Password
-          </span>
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white transition-colors duration-200"
-          />
-        </label>
-        <label className="flex items-center gap-2">
-          <input
-            type="checkbox"
-            className="w-4 h-4 border border-gray-300 dark:border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-black dark:focus:ring-white transition-colors duration-200"
-          />
-          <span className="text-gray-700 dark:text-gray-400">Remember me</span>
-        </label>
+        <Input label="Email" type="email" placeholder="your@email.com" />
+        <Input label="Password" type="password" placeholder="Password" />
+        <Checkbox label="Remember me" />
         <button
           type="submit"
           className="w-full px-4 py-2 rounded-md text-white bg-black hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-300 transition-colors duration-200 cursor-pointer"
@@ -42,6 +22,12 @@ export const LoginForm = () => {
           Log in
         </button>
       </form>
+      <p className="text-center text-gray-700 dark:text-gray-400">
+        Dont have an account?{" "}
+        <Link href="/login" className="text-gray-500 hover:underline">
+          Sing up here
+        </Link>
+      </p>
     </div>
   );
 };
